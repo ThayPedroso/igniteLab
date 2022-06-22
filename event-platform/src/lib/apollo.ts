ed834ac6531f40509fd1a1fd7e8979abeca22f83
Thayla Pedroso <thayla.pedroso88@gmail.com>
2022-06-21 21:31:01 -0300

    sidebar

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: '',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oo8egh0mw201z4eh9o1tnf/master',
     cache: new InMemoryCache()
 })
